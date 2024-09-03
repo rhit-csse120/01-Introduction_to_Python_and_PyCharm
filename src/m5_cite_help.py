95ec432dc4b02dc20eb274d0b925c30839dff01a
mutchler <mutchler@rose-hulman.edu>
2024-09-03 10:26:12 -0400

    File for demonstrating how to cite work (and also an excuse for discussing the Academic Integrity policy).

---
+++ src/m5_cite_help.py
@@ -0,0 +1,47 @@
+"""
+Do this module WITH (or as directed by) YOUR INSTRUCTOR.
+It lets you practice citing help as part of Academic Integrity.
+
+In this course, when you get help on a module from a person(s),
+you must CITE the help that you received by listing their names
+on the relevant line below (line XXX).  Note that:
+  -- It is perfectly OK (indeed, expected!) for you to get help
+       from people in working on the modules in this course!
+       Conversations are what create a DEEP understanding of the material!
+  -- For simplicity, cite any help you get from your another student,
+       a course assistant, your instructor, your grandmother -- whatever!
+       If in doubt, cite the help.
+  -- Your grade is NEVER reduced by citing help.
+
+Authors: David Mutchler, Rachel Krohn, Dave Fisher, Shawn Bohner, Sriram Mohan,
+         Amanda Stouder, Vibha Alangar, Mark Hays, Dave Henthorn, Matt Boutell,
+         Scott McClellan, Yiji Zhang, Mohammed Noureddine, Steve Chenoweth,
+         Derek Whitley, Curt Clifton, Valerie Galluzzi, their colleagues and
+         PUT_YOUR_NAME_HERE.
+"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""
+Academic Integrity: I got help on this module from:
+         PUT_HERE_THE_NAMES_OF_PEOPLE_WHO_HELPED_YOU_ON_THIS_MODULE_(IF_ANY).
+"""  # TODO: If you got help from anyone on this module, list their names here.
+
+###############################################################################
+# TODO: 2.
+#  ASK A FRIEND FOR HELP  ** OR **  GIVE A FRIEND HELP (as directed by your
+#  instructor) in completing this _TODO_.
+#  _
+#  In this comment, write what   BDFL   stands for when referring to
+#  Guido van Rossum (the inventor of Python).
+#    BDFL stands for PUT_YOUR_ANSWER_HERE.
+################################################################################
+
+################################################################################
+# TODO: 3.
+#  ASK A FRIEND FOR HELP  ** OR **  GIVE A FRIEND HELP (as directed by your
+#  instructor) in completing this _TODO_.
+#  _
+#  Below this comment, write a SINGLE-LINE statement
+#  that prints "Hi" 1,000 times on a single (long) line.
+#  Also, experiment with the "Wrap Text" and "Scroll to End" buttons.
+################################################################################
+
+print("Hi" * 1000)
