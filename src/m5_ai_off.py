fdc05e7264e44d973ae3a14ca9e337582a6c7490
mutchler <mutchler@rose-hulman.edu>
2026-06-01 09:51:23 -0400

    Added an exercise to turn OFF MULTI-line auto-completion.

---
+++ src/m5_ai_off.py
@@ -0,0 +1,57 @@
+"""
+Do this module WITH (or as directed by) YOUR INSTRUCTOR.
+It helps you set up your PyCharm to AVOID giving MULTI-line "AI" help.
+
+Authors: David Mutchler, Rachel Krohn, Dave Fisher, Shawn Bohner, Sriram Mohan,
+         Amanda Stouder, Vibha Alangar, Mark Hays, Dave Henthorn, Matt Boutell,
+         Scott McClellan, Yiji Zhang, Mohammed Noureddine, Steve Chenoweth,
+         Claude Anderson, Michael Wollowski, Chandan Rupakheti,
+         Derek Whitley, Curt Clifton, Valerie Galluzzi, their colleagues and
+         PUT_YOUR_NAME_HERE.
+"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+
+"""
+Academic Integrity: I got help on this module from:
+         PUT_HERE_THE_NAMES_OF_PEOPLE_WHO_HELPED_YOU_ON_THIS_MODULE_(IF_ANY).
+"""  # TODO: If you got help from anyone on this module, list their names here.
+
+###############################################################################
+# TODO: 2.
+#  WITH YOUR INSTRUCTOR'S HELP/GUIDANCE, experience MULTI-line auto-completion,
+#  as follows:
+#    1. Find the line BELOW this comment that says
+#          total = 0
+#    2. Place the cursor at the END of that line (after the 0).
+#    3. Press the Enter (
+#    Return) key.
+#  You MAY see SEVERAL lines of code suggested for you.
+#  If so, you have the MULTI-line auto-completion turned on.
+###############################################################################
+
+
+def calculate_sum(n):
+    """ Return 0 + 1 + 2 + ... + n"""
+    total = 0
+
+
+###############################################################################
+# TODO: 3.
+#  We want to turn the MULTI-line auto-completion OFF,
+#  as it provides TOO much help and detracts from learning.
+#  Do so as follows, WITH YOUR INSTRUCTOR'S HELP:
+#     1. File ~ Settings
+#     2. Expand the   Editor   item in the leftmost window,
+#          then expand the   General   item,
+#          then select the   Inline Completion   item.
+#     3. In the window to the right, UN-check the
+#          Offer multi-line suggestions
+#        option.
+#     4. Select the  OK  button.
+#  _
+#  Then repeat _TODO_ 2 above.  This time it should offer only
+#  a SINGLE line of auto-completion.
+#  _
+#    ** GET HELP AS NEEDED IF YOU ARE UNSURE WHETHER OR NOT     **
+#    ** YOU HAVE IN FACT TURNED OFF MULTI-LINE AUTO-COMPLETION, **
+#    ** SINCE USING IT IS A VIOLATION OF THE INTEGRITY POLICY.  **
+################################################################################
